787b0476646126ae7e5b244c0c3c3284a28905d8
geranobeks <gerano007@live.nl>
2017-09-28 11:15:09 +0200

    opdracht 1 compleet en en dat zonder afkijken of hulp vragen 👍🏾

---
+++ src/h08/Practice1.java
@@ -0,0 +1,49 @@
+package h08;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Practice1 extends Applet {
+    Button knop;
+    TextField tekstvak;
+    Button dop;
+    String halo;
+
+
+    public void init() {
+        setSize(600, 300);
+        tekstvak = new TextField("", 30);
+        knop = new Button("OK");
+        knop.addActionListener(new KnopListener());
+        dop = new Button("Reset");
+        dop.addActionListener(new Wis());
+        add(tekstvak);
+        add(knop);
+        add(dop);
+        halo = "";
+
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("typ iets lang in het tekstvakje en klik op OK: " + halo, 50, 60);
+    }
+
+    class KnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            halo = new String();
+            halo = tekstvak.getText();
+
+            repaint();
+        }
+    }
+
+    class Wis implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            tekstvak.setText("");
+            repaint();
+        }
+    }
+}
+
